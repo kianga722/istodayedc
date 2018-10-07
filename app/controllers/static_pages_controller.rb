2411198d3ae5f07c5779db559d66f3ecc8973d09
Top Tester <json900064@gmail.com>
2018-10-07 19:32:57 -0400

    Added style randomizations

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,69 @@
 class StaticPagesController < ApplicationController
 
     def home
+        @zoo = false
+
+        today = Date.current
+
+        zooday = [Date.new(2019,8,31),
+                  Date.new(2019,8,30),
+                  Date.new(2019,9,1)]
+
+        #zooday = [Date.new(2019,8,31),
+        #          Date.new(2019,8,30),
+        #          Date.new(2019,9,1),
+        #          Date.new(2018,10,7)]
+
+        @zoo = true if zooday.include?(Date.current)
+
+        #Random Styling
+        fonts = ["serif", "sans-serif", 
+                 "Arial", "Arial Black", "Gadget" "Helvetica", "Comic Sans MS", "cursive", "Impact", "Charcoal", "Lucida Sans Unicode", "Lucida Grande", "Tahoma", "Geneva", "Trebuchet MS", "Verdana", "Courier New", "Courier", "Lucida Console", "Monaco", "monospace"]
+        weights = [100, 400, 700]
+        italic = %w(normal italic oblique)
+        textdec = %w(none underline)
+        textdecstyle = %w(solid double dotted dashed wavy)
+        borderstyle = %w(none dotted dashed solid double groove ridge inset outset)
+
+
+        @font = fonts.sample
+        @fweight = weights.sample
+        @italic = italic.sample
+
+        @textd = textdec.sample
+        @textdcolor = SecureRandom.hex(3)
+        @textdstyle = textdecstyle.sample
+
+        @color = SecureRandom.hex(3)
+        @bgcolor = SecureRandom.hex(3)
+
+        @bordertl = rand(2000)
+        @bordertr = rand(2000)
+        @borderbl = rand(2000)
+        @borderbr = rand(2000)
+
+        @bordercolort = SecureRandom.hex(3)
+        @bordercolorr = SecureRandom.hex(3)
+        @bordercolorb = SecureRandom.hex(3)
+        @bordercolorl = SecureRandom.hex(3)
+
+        @borderwidtht = rand(100)
+        @borderwidthr = rand(100)
+        @borderwidthb = rand(100)
+        @borderwidthl = rand(100)
+
+        @borderstylet = borderstyle.sample
+        @borderstyler = borderstyle.sample
+        @borderstyleb = borderstyle.sample
+        @borderstylel = borderstyle.sample
+
+
+
+        @paddingt = rand(100)
+        @paddingr = rand(100)
+        @paddingb = rand(100)
+        @paddingl = rand(100)
+        
     end
 
 end
